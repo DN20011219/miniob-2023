0684ef13caf56797b5aad22559bf32cb8115c3fb
dingning14 <dingning14@meituan.com>
2024-08-27 18:17:23 +0800

    update drop table

--- src/observer/storage/db/db.cpp
+++ src/observer/storage/db/db.cpp
@@ -104,6 +104,34 @@ RC Db::create_table(const char *table_name, int attribute_count, const AttrInfoS
 RC Db::drop_table(const char *table_name)
 {
   // TODO:dingning
+  RC rc = RC::SUCCESS;
+  // check table_name
+  if (opened_tables_.count(table_name) != 0) {
+    LOG_WARN("%s has been opened before.", table_name);
+    return RC::SCHEMA_TABLE_EXIST;
+  }
+
+
+  std::string table_file_path = table_meta_file(path_.c_str(), table_name);
+  
+  // 删除table header
+  // 删除table data
+  // 删除table index
+  // 删除视图。。。。
+  // 文件路径可以移到Table模块
+  
+  Table *table = new Table();
+  int32_t table_id = next_table_id_++;
+  // rc = table->create(table_id, table_file_path.c_str(), table_name, path_.c_str(), attribute_count, attributes);
+  if (rc != RC::SUCCESS) {
+    LOG_ERROR("Failed to create table %s.", table_name);
+    delete table;
+    return rc;
+  }
+
+  opened_tables_[table_name] = table;
+  LOG_INFO("Create table success. table name=%s, table_id:%d", table_name, table_id);
+  return RC::SUCCESS;
 }
 
 Table *Db::find_table(const char *table_name) const
